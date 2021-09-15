9e2ebca53b983b1441d537e2c877255264603901
Filip Sosnowski <sosnowsf@chuck.tchpc.tcd.ie>
2021-09-15 18:43:42 +0100

    Added 3d mpi-io

--- ising3d/mpi_io.h
+++ ising3d/mpi_io.h
@@ -2,6 +2,7 @@
 
 void save_grid(int g[x][y][z], int, int);
 void save_grid2d(int g[x][y][z], int, int, int, int);
+void save_grid3d(int g[x][y][z], int, int, int, int, int ,int);
 void load_grid(int g[x][y][z], int, int);
 void load_grid2d(int g[x][y][z], int, int, int, int);
-
+void load_grid3d(int g[x][y][z], int, int, int, int, int, int);
